ad210b1343de77b462957c1dd1462797d9d33ff8
chandru <chandru190703@gmail.com>
2024-10-18 12:57:33 +0530

    add the new variable age

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
-def main (name):
-    print ("hello guys greets by", name, "from main")
+def main (name, age):
+    print ("hello guys greets by", name, "age is :", age, "from main")
 
 name = "chandru-programista"
+age = 21
 
-main(name)
+main(name, age)
